f779f41e56819202763e1795f22a08765cdeea4d
Thomas Bocandé <thomasbocande@gmail.com>
2024-01-13 19:59:48 +0100

    simplification de Hdfs Sever

--- hagidoop/src/hdfs/HdfsServer.java
+++ hagidoop/src/hdfs/HdfsServer.java
@@ -3,16 +3,98 @@ package hdfs;
 import java.io.*;
 import java.net.*;
 
-//rajouter des méthodes read, write, delete
+import interfaces.FileReaderWriterImpl;
+import interfaces.KV;
 
-public class HdfsServer {
+public class HdfsServer extends Thread {
+    private Socket client;
+
+    public HdfsServer(Socket s) {
+        this.client = s;
+    }
+
+    public void run () {
+		try {
+            Socket clientSocket = this.client;
+
+            ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            while (true) {
+                String request = reader.readLine();
+
+                if (request.startsWith("DELETE")) {
+                    String[] tokens = request.split(" ");
+                    String fname = tokens[1];
+                    // Supprimer le fichier fname
+                    String response = fname + " DELETED";
+                    outputStream.writeObject(response);
+                } else if (request.startsWith("WRITE")) {
+                    String[] tokens = request.split(" ");
+                    String fname = tokens[1];
+                    int fmt = Integer.parseInt(tokens[2]);
+
+                    if (fmt == 0) {
+                        // Ecrire le fragment sous le format txt
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(fname, true));
+                        String fragment = inputStream.readObject().toString();
+
+                        writer.write(fragment);
+                        writer.close();
+                    } else if (fmt == 1) {
+                        // Ecrire le fragment sous le format kv
+                        KV fragment = (KV) inputStream.readObject();
+                        FileReaderWriterImpl file = new FileReaderWriterImpl(fname);
+                        file.open("w");
+                        file.write(fragment);
+                        file.close();
+                    } else {
+                        System.out.println("Unknown format: " + fmt);
+                    }
+                    String response = fname + " WRITTEN";
+                    outputStream.writeObject(response);
+                } else if (request.startsWith("READ")) {
+                    String[] tokens = request.split(" ");
+                    String fname = tokens[1];
+
+                    FileReaderWriterImpl file = new FileReaderWriterImpl(fname);
+                    file.open("r");
+
+                    String response = "Readind file " + fname + " ...";
+                    outputStream.writeObject(response);
+                    
+                    // Lire le fichier fname
+                    KV content = file.read();
+
+                    while (content.k != null) {
+                        outputStream.writeObject(content);
+                        content = file.read();
+                    }
+
+                    content = null;
+                    outputStream.writeObject(content);
+
+                    String end = "END OF FILE";
+                    outputStream.writeObject(end);
+
+                    file.close();
+                } else {
+                    System.out.println("Unknown request: " + request);
+                }
+            }
+		} catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+
     public static void main (String args[]) {
 		try {
 			ServerSocket serverSocket = new ServerSocket(8080);
 		    while (true) {
 			    Socket clientSocket = serverSocket.accept();
-                ThreadServer thread = new ThreadServer(clientSocket);
-                thread.start();
+                HdfsServer hdfsServer = new HdfsServer(clientSocket);
+                hdfsServer.start();
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
