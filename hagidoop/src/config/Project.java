acc4a3710c8fdfbe9cd3997c5aaaee110be29f5f
Thomas Bocandé <thomasbocande@gmail.com>
2024-01-15 19:25:51 +0100

    màj

--- hagidoop/src/config/Project.java
+++ hagidoop/src/config/Project.java
@@ -6,15 +6,17 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class Project {
-    public static final String PATH = "";
+    public static String PATH = "";
+    public static HashMap<Integer, String> servers = new HashMap<Integer, String>();
+
+    public Project() {}
 
     /*
 	 * readConfigFile : lit le fichier de configuration et remplit les listes machines et ports
 	 */
-	public HashMap<Integer, String> readConfigFile() throws IOException {
+	private HashMap<Integer, String> readConfigFile() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(PATH + "config/config.txt"));
         String line;
-        HashMap<Integer, String> servers = new HashMap<Integer, String>();
 
         while ((line = reader.readLine()) != null) {
             if (line.startsWith("#")) { continue; }
@@ -26,5 +28,14 @@ public class Project {
         }
         reader.close();
         return servers;
+    }
+
+    public static void main(String[] args) {
+        Project p = new Project();
+        try {
+            p.readConfigFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- hagidoop/src/daemon/JobLauncher.java
+++ hagidoop/src/daemon/JobLauncher.java
@@ -1,13 +1,12 @@
 package daemon;
 
-import java.io.IOException;
 import java.rmi.Naming;
 
 import interfaces.FileReaderWriter;
 import interfaces.MapReduce;
 import interfaces.NetworkReaderWriter;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import java.util.Map;
 
 import config.Project;
 
@@ -16,11 +15,8 @@ public class JobLauncher {
 	public static void startJob (MapReduce mr, int format, String fname) {
 		Project project = new Project();
 		try {
-			List<Integer> ports = project.readConfigFile().keySet().stream().collect(Collectors.toList());
-			List<String> hosts = project.readConfigFile().values().stream().collect(Collectors.toList());			
-		
-			for (int i = 0; i < ports.size(); i++) {
-				WorkerImpl worker = (WorkerImpl) Naming.lookup("rmi://" + hosts.get(i) + ":" + ports.get(i) + "/Worker");
+			for (Map.Entry<Integer,String> e: project.servers.entrySet()) {
+				WorkerImpl worker = (WorkerImpl) Naming.lookup("rmi://" + e.getValue() + ":" + e.getKey() + "/Worker");
 				worker.runMap(mr, new FileReaderWriter(fname), new NetworkReaderWriter());
 			}
 		} catch (Exception e) {
