edd14bcfd967a45de877baf4faf2016e381357bd
Thomas Bocandé <thomasbocande@gmail.com>
2024-01-16 02:57:34 +0100

    config moved

--- hagidoop/src/daemon/Hagidoop.java
+++ hagidoop/src/daemon/Hagidoop.java
@@ -1,5 +1,11 @@
 package daemon;
 
+import config.Project;
+
 public class Hagidoop {
-    
+    public static void main(String args[]) {
+        Project projet = new Project();
+
+        projet.setPath(args[0]);
+    }
 }
